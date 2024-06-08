e568b76d8953bec00ae57ccdc6041d8c50bc6b21
Ados <aden.huen@gmail.com>
2024-06-08 18:14:42 +0800

    feat: low cost strategy

--- src/app.py
+++ src/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 from llm import query
 from flask_cors import CORS
+from db_query import db_query
 
 app = Flask(__name__)
 CORS(app)
@@ -14,7 +15,8 @@ def hello():
 @app.route('/meal-plan', methods=['POST'])
 def meal_plan():
     user_input = request.json['user_input']
-    response = query.meal_plan_query(user_input)
+    ingredients = db_query.get_items_JSON()
+    response = query.meal_plan_query(ingredients, user_input)
     return response.content
 
 
--- src/llm/query.py
+++ src/llm/query.py
@@ -5,20 +5,11 @@ client = Groq(
     api_key=os.environ.get("GROQ_API_KEY"),
 )
 
-def get_low_cost_strategy(prices):
-    prompt = "You are a meal planner, skilled in creating balanced and nutritious meals plans for the lowest cost possible. \n"
-    prompt += "Given the following ingredients and their cost:\n"
-
-    prompt += "Assuming common household items are available, give me a recipe that fulfills the following request, ignoring items that are costly:\n"
-    return {
-        "role": "system",
-        "content": prompt
-    }
 
 
-def meal_plan_query(user_input):
+def meal_plan_query(ingredients, user_input):
     groq_input = {"role": "user", "content": user_input}
-    strategy = get_low_cost_strategy(prices)
+    strategy = get_low_cost_strategy(ingredients)
 
     meal_plan = client.chat.completions.create(
         messages=[
@@ -28,3 +19,20 @@ def meal_plan_query(user_input):
         model="llama3-8b-8192",
     )
     return meal_plan.choices[0].message
+
+def get_low_cost_strategy(ingredient):
+    prompt = "You are a meal planner, skilled in creating balanced and nutritious meals plans for the lowest cost possible. \n"
+    prompt += "Given the following ingredients and their cost:\n"
+    prompt += get_ingredients(ingredient)
+    prompt += "Assuming common household items are available, give me a recipe that fulfills the following request, ignoring items that are costly:\n"
+    return {
+        "role": "system",
+        "content": prompt
+    }
+
+def get_ingredients(ingredients):
+    ingredients_str = ""
+    for ingredient in ingredients:
+        print(ingredient)
+        ingredients_str += f"{ingredient.get('name')}, ${ingredient.get('cost')}\n"
+    return ingredients_str
