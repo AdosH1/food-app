316f6e68264c572ab21d288119078e247795954f
Ados <aden.huen@gmail.com>
2024-06-08 17:23:06 +0800

    wip: low cost strategy

--- src/llm/query.py
+++ src/llm/query.py
@@ -5,14 +5,24 @@ client = Groq(
     api_key=os.environ.get("GROQ_API_KEY"),
 )
 
-meal_plan_context = {"role": "system", "content": "You are a meal planner, skilled in creating balanced and nutritious meals plans for individuals with dietary restrictions."}
+def get_low_cost_strategy(prices):
+    prompt = "You are a meal planner, skilled in creating balanced and nutritious meals plans for the lowest cost possible. \n"
+    prompt += "Given the following ingredients and their cost:\n"
+
+    prompt += "Assuming common household items are available, give me a recipe that fulfills the following request, ignoring items that are costly:\n"
+    return {
+        "role": "system",
+        "content": prompt
+    }
+
 
 def meal_plan_query(user_input):
     groq_input = {"role": "user", "content": user_input}
+    strategy = get_low_cost_strategy(prices)
 
     meal_plan = client.chat.completions.create(
         messages=[
-            meal_plan_context, 
+            strategy, 
             groq_input,
           ],
         model="llama3-8b-8192",
